62f84060197239a36ef035509840290c1d440a1f
tabrown3 <t-a-brown@hotmail.com>
2022-10-23 20:26:47 -0500

    add spi controller to sd card controller

--- sd_card_controller.v
+++ sd_card_controller.v
@@ -3,12 +3,15 @@ module sd_card_controller (
     input execute, // on execute, perform op
     input clk, // master clk
     input [25:0] sector_address, // multiply this by 512 to get byte address
+    input miso,
     input [7:0] byte_in, // byte to write
+    output reg cs = 1'b1, // chip select
     output reg [7:0] byte_out = 8'hff, // holds byte being read
+    output mosi,
     output reg finished_byte = 1'b0, // indicates a byte has been written or read
-    output reg finished_sector = 1'b0 // indicates the op is finished
+    output reg finished_sector = 1'b0, // indicates the op is finished
+    output spi_clk
 );
-
     localparam UNINITIALIZED = 5'h00;
     localparam SEND_INIT_NO_OPS = 5'h01;
     localparam SEND_CMD0 = 5'h02;
@@ -19,6 +22,19 @@ module sd_card_controller (
     localparam AWAIT_ACMD41_RES = 5'h07; // don't forget to handle error
 
     reg executing = 1'b0;
+    reg execute_txrx = 1'b0;
+    wire txrx_finished;
+
+    spi_controller SPI_CONT(
+        .execute(execute_txrx),
+        .clk(clk),
+        .miso(miso),
+        .out_word(byte_out),
+        .spi_clk(spi_clk),
+        .mosi(mosi),
+        .in_word(byte_in),
+        .finished(txrx_finished)
+    );
 
     always @(negedge clk) begin
 
