4343b4df876575e1e67d30da5dc6f452730924a0
tabrown3 <t-a-brown@hotmail.com>
2022-10-29 12:19:47 -0500

    start on send cmd0

--- sd_card_controller.v
+++ sd_card_controller.v
@@ -37,13 +37,21 @@ module sd_card_controller (
     reg execute_txrx = 1'b0;
     integer target_count = 0;
     integer cur_count = 0;
+    reg [5:0] cur_cmd;
+    reg [31:0] cur_args;
+    reg [7:0] cur_crc;
+    reg initialize_state = 1'b0;
+    reg send_no_op = 1'b0;
     wire txrx_finished;
     wire txrx_busy;
     wire [7:0] tx_byte;
     wire [7:0] rx_byte;
+    wire [47:0] full_cmd;
+    wire [5:0] cmd_ind;
 
     assign busy = executing;
-    assign tx_byte = SEND_INIT_NO_OPS ? 8'hff : outgoing_byte;
+    assign full_cmd = {1'b0, 1'b1, cur_cmd, cur_args, cur_crc};
+    assign tx_byte = send_no_op ? 8'h00 : full_cmd;
 
     spi_controller SPI_CONT(
         .execute(execute_txrx),
@@ -65,25 +73,56 @@ module sd_card_controller (
         case (cur_state)
             UNINITIALIZED: begin
                 cur_state <= SEND_INIT_NO_OPS;
-                target_count <= 80;
-                cur_count <= 0;
-                executing <= 1'b1; // let controllers know we're busy
-                execute_txrx <= 1'b1; // start executing txrx sequences
+                initialize_state <= 1'b1;
             end
             SEND_INIT_NO_OPS: begin
-                if (cur_count >= target_count) begin // once 80 blank bytes have been sent
-                    if (txrx_finished) begin // once current sequence completes
-                        cur_state <= SEND_CMD0; // change state
-                        target_count <= 0;
-                        cur_count <= 0;
+                if (initialize_state) begin
+                    initialize_state <= 1'b0;
+
+                    target_count <= 80;
+                    cur_count <= 0;
+                    send_no_op <= 1'b1; // we're just running the clk, no cmd
+
+                    executing <= 1'b1; // let controllers know we're busy
+                    execute_txrx <= 1'b1; // start executing txrx sequences
+                end else begin
+                    if (cur_count >= target_count) begin // once 80 blank bytes have been sent
+                        if (txrx_finished) begin // once current sequence completes
+                            cur_state <= SEND_CMD0; // change state
+                            initialize_state <= 1'b1;
+                        end
+                    end else if (txrx_finished) begin // once current sequence completes
+                        cur_count <= cur_count + 1; // increment count
+                        execute_txrx <= 1'b1; // start next txrx sequence
                     end
-                end else if (txrx_finished) begin // once current sequence completes
-                    cur_count <= cur_count + 1; // increment count
-                    execute_txrx <= 1'b1; // start next txrx sequence
                 end
             end
             SEND_CMD0: begin
-                
+                if (initialize_state) begin
+                    initialize_state <= 1'b0;
+
+                    target_count <= 6;
+                    cur_count <= 0;
+                    send_no_op <= 1'b0;
+
+                    cur_cmd <= CMD0;
+                    cur_args <= {32{1'b1}};
+                    cur_crc <= 8'h95;
+
+                    execute_txrx <= 1'b1;
+                end else begin
+                    if (cur_count >= target_count) begin
+                        if (txrx_finished) begin // once current sequence completes
+                            cur_state <= AWAIT_CMD0_RES; // change state
+                            initialize_state <= 1'b1;
+                        end
+                    end else if (txrx_finished) begin
+                        cur_count <= cur_count + 1; // increment count
+                        execute_txrx <= 1'b1;
+                    end
+                end
+            end
+            AWAIT_CMD0_RES: begin
             end
             default: begin
                 cur_state <= SEND_CMD0;
