0f235b20147d9edaeae3172af190b7c05f90bf40
tabrown3 <t-a-brown@hotmail.com>
2022-11-11 12:49:59 -0600

    modify ports

--- fat32_controller.v
+++ fat32_controller.v
@@ -1,21 +1,26 @@
 module fat32_controller (
+    input [63:0] filename, // 8 bytes for name
+    input [23:0] extension, // 3 bytes for extension
     input op_code,
     input clk,
-    input miso,
     input [7:0] outgoing_byte,
     input btn,
-    output cs,
     output [7:0] incoming_byte,
-    output mosi,
     output finished_byte,
     output finished_block,
-    output spi_clk,
-    output busy
+    output busy,
+
+    // pass-through (neither used nor modified at this level)
+    input miso,
+    output cs,
+    output mosi,
+    output spi_clk
 );
     reg sd_op_code = 1'b0;
     reg sd_execute = 1'b0;
     reg [31:0] block_address = {32{1'b0}};
-    reg [7:0] sd_outgoing_byte = 8'haa;
+    reg [7:0] sd_outgoing_byte = 8'haa; // this is dummy data
+    reg sd_btn = 1'b1;
     wire [7:0] sd_incoming_byte;
     wire sd_finished_byte;
     wire sd_finished_block;
@@ -28,7 +33,7 @@ module fat32_controller (
         .block_address(block_address),
         .miso(miso),
         .outgoing_byte(sd_outgoing_byte),
-        .btn(btn),
+        .btn(sd_btn),
         .cs(cs),
         .incoming_byte(sd_incoming_byte),
         .mosi(mosi),
